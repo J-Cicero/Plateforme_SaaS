156c4592a8d3022b1a3834b58c700d25761d2bbf
J-Cicero <prudencioworou2006@gmail.com>
2026-02-17 11:01:47 +0000

    initial commit: nouveau projet

---
+++ src/test/java/com/saas/plateform/PlateformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saas.plateform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlateformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
